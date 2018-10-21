a1b29b9463858f943c18609a3376055ea5c08964
Matt Palmer <mpalmer@hezmatt.org>
2018-10-22 08:50:08 +1100

    Augmentations for simplecov
    1. Report on files which are incompletely covered.  This saves everyone having to fire up a web browser
       just to find out it's not *actually* their code that's uncovered.
    
    2. Fail the test suite if code coverage drops.  How exactly simplecov figures out what "dropped"
       means I'm not sure, but it's better than nothing, anyway.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,27 @@ require 'simplecov'
 SimpleCov.start do
   add_filter('spec')
 end
+SimpleCov.refuse_coverage_drop
+
+class ListIncompletelyCoveredFiles
+  def format(result)
+    incompletes = result.files.select { |f| f.covered_percent < 100 }
+
+    unless incompletes.empty?
+      puts
+      puts "Files with incomplete test coverage:"
+      incompletes.each do |f|
+        printf "    %2.02f%%    %s", f.covered_percent, f.filename
+      end
+      puts; puts
+    end
+  end
+end
+
+SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  ListIncompletelyCoveredFiles
+])
 
 RSpec.configure do |config|
   config.fail_fast = true
