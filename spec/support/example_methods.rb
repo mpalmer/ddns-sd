ea3464a07bb7c2386e007ede8b3e1c72fbb34b10
Neil Lalonde <neillalonde@gmail.com>
2019-01-21 10:46:35 -0500

    move some files to spec/support

--- spec/support/example_methods.rb
+++ spec/support/example_methods.rb
@@ -4,7 +4,7 @@ require 'yaml'
 
 module ExampleMethods
   def container_fixture(name)
-    data = YAML.load_file(File.expand_path("../fixtures/container_data/#{name}.yml", __FILE__))
+    data = YAML.load_file(File.expand_path("../../fixtures/container_data/#{name}.yml", __FILE__))
     Docker::Container.send(:new, Docker::Connection.new("unix:///", {}), data)
   end
 
@@ -13,7 +13,7 @@ module ExampleMethods
   end
 
   def dns_record_fixture(name)
-    data = YAML.load_file(File.expand_path("../fixtures/dns_records/#{name}.yml", __FILE__))
+    data = YAML.load_file(File.expand_path("../../fixtures/dns_records/#{name}.yml", __FILE__))
     data.map { |d| DDNSSD::DNSRecord.new(d["name"], d["ttl"], d["type"], *d["data"]) }
   end
 
@@ -41,12 +41,12 @@ module ExampleMethods
   end
 
   def route53_response_fixture(name)
-    YAML.load_file(File.expand_path("../fixtures/route53_responses/#{name}.yml", __FILE__))
+    YAML.load_file(File.expand_path("../../fixtures/route53_responses/#{name}.yml", __FILE__))
   end
 
   def azure_response_fixture(name)
     client = Azure::Dns::Mgmt::V2018_03_01_preview::DnsManagementClient.new
-    json = File.read(File.expand_path("../fixtures/azure_responses/#{name}.json", __FILE__))
+    json = File.read(File.expand_path("../../fixtures/azure_responses/#{name}.json", __FILE__))
     request_mapper = Azure::Dns::Mgmt::V2018_03_01_preview::Models::RecordSet.mapper
     records = JSON.parse(json).map { |v| client.deserialize(request_mapper, v) }
     records.map { |rs|
